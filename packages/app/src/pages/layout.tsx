8deb821d269024f36f80fd93ba580dfbdb5cafb8
David Hill <iamdavidhill@gmail.com>
2026-01-18 00:57:21 +0000

    Revert "fix: increase max-width of session name tooltip"
    This reverts commit be44abfc7085c0fe1ffdcf679f1f4342645988a6.

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -1119,8 +1119,7 @@ export default function Layout(props: ParentProps) {
               placement="top-start"
               value={props.session.title}
               gutter={0}
-              openDelay={3000}
-              contentStyle={{ maxWidth: "640px" }}
+              openDelay={2000}
               class="grow-1 min-w-0"
             >
               <InlineEditor
