be44abfc7085c0fe1ffdcf679f1f4342645988a6
David Hill <iamdavidhill@gmail.com>
2026-01-18 00:51:35 +0000

    fix: increase max-width of session name tooltip

--- packages/app/src/pages/layout.tsx
+++ packages/app/src/pages/layout.tsx
@@ -1119,7 +1119,8 @@ export default function Layout(props: ParentProps) {
               placement="top-start"
               value={props.session.title}
               gutter={0}
-              openDelay={2000}
+              openDelay={3000}
+              contentStyle={{ maxWidth: "640px" }}
               class="grow-1 min-w-0"
             >
               <InlineEditor
