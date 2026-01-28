ba1716ae2162be6d65a67f1d786fb34ef4502b6b
zerone0x <hi@trine.dev>
2026-01-29 06:44:35 +0800

    fix(ui): allow KaTeX inline math to be followed by punctuation (#11033)

--- packages/ui/src/context/marked.tsx
+++ packages/ui/src/context/marked.tsx
@@ -475,6 +475,7 @@ export const { use: useMarked, provider: MarkedProvider } = createSimpleContext(
       },
       markedKatex({
         throwOnError: false,
+        nonStandard: true,
       }),
       markedShiki({
         async highlight(code, lang) {
