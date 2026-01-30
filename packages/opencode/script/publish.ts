802263fc3ef51ff374faaca06cf225978a0b865f
Dax Raad <d@ironbay.co>
2026-01-30 15:17:55 -0500

    commit

--- packages/opencode/script/publish.ts
+++ packages/opencode/script/publish.ts
@@ -37,7 +37,6 @@ await Bun.file(`./dist/${pkg.name}/package.json`).write(
   ),
 )
 
-/*
 const tasks = Object.entries(binaries).map(async ([name]) => {
   if (process.platform !== "win32") {
     await $`chmod -R 755 .`.cwd(`./dist/${name}`)
@@ -53,7 +52,6 @@ const platforms = "linux/amd64,linux/arm64"
 const tags = [`${image}:${version}`, `${image}:${Script.channel}`]
 const tagFlags = tags.flatMap((t) => ["-t", t])
 await $`docker buildx build --platform ${platforms} ${tagFlags} --push .`
-*/
 
 // registries
 if (!Script.preview) {
@@ -65,7 +63,6 @@ if (!Script.preview) {
 
   const [pkgver, _subver = ""] = Script.version.split(/(-.*)/, 2)
 
-  /*
   // arch
   const binaryPkgbuild = [
     "# Maintainer: dax",
@@ -179,7 +176,6 @@ if (!Script.preview) {
       }
     }
   }
-  */
 
   // Homebrew formula
   const homebrewFormula = [
